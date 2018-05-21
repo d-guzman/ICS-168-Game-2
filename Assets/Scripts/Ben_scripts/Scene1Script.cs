d07682dd34168de254d8edf47af5f582debca2a9
Zaphero1 <Brgibson@uci.edu>
2018-05-20 20:42:21 -0700

    Added Main Menu
    Buttons are established.

---
+++ Assets/Scripts/Ben_scripts/Scene1Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene1Script : MonoBehaviour {
+    public string Scene1Name;
+
+
+	public void OnClick () {
+        SceneManager.LoadScene(Scene1Name);
+    }
+}
